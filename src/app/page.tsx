4188ea1d0068cc1ee3989dd7351ad0fea0abd4ec
levaniakobidze <levaniakobidze25@gmail.com>
2023-09-15 22:50:01 +0400

    style: simple style changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,36 +4,53 @@ import styles from "./page.module.css";
 import HeroBanner from "../components/HeroBanner/HeroBanner";
 import ReviewCard from "../components/ReviewCard/ReviewCard";
 import { useAppSelector } from "../redux/hooks";
+import { useState } from "react";
 
 export default function Home() {
   const { reviews } = useAppSelector((state: any) => state.reviews);
+  const [imgindx, setImgindx] = useState(0);
+  const [images, setImages] = useState([
+    "https://wallpaperaccess.com/full/1077148.jpg",
+    "https://wallpaperaccess.com/full/1077153.jpg",
+    "https://wallpaperaccess.com/full/1077173.jpg",
+    "https://wallpaperaccess.com/full/1077197.jpg",
+    "https://wallpaperaccess.com/full/1988347.jpg",
+  ]);
+
+  setTimeout(() => {
+    const random = Math.floor(Math.random() * images.length);
+    setImgindx(random);
+  }, 7000);
+
   return (
-    <main className={styles.main}>
-      <HeroBanner img="https://images8.alphacoders.com/131/1312070.jpg" />
-      <div
-        style={{
-          marginTop: "50px",
-          display: "flex",
-          flexWrap: "wrap",
-          gap: "50px",
-        }}
-      >
-        <div>
-          <h2 className={styles.section_title}>Latest reviews</h2>
-          <div className={styles.latest_reviews_list}>
-            {reviews &&
-              reviews.map((review: any, index: number) => {
-                return (
-                  <ReviewCard
-                    key={review._id}
-                    img={review.poster}
-                    description={review.description}
-                  />
-                );
-              })}
+    <>
+      <HeroBanner img={images[1]} />
+      <main className={styles.main}>
+        <div
+          style={{
+            marginTop: "50px",
+            display: "flex",
+            flexWrap: "wrap",
+            gap: "50px",
+          }}
+        >
+          <div>
+            <h2 className={styles.section_title}>Latest reviews</h2>
+            <div className={styles.latest_reviews_list}>
+              {reviews &&
+                reviews.map((review: any, index: number) => {
+                  return (
+                    <ReviewCard
+                      key={review._id}
+                      img={review.poster}
+                      description={review.description}
+                    />
+                  );
+                })}
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 }
--- src/components/HeroBanner/HeroBanner.tsx
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -4,7 +4,10 @@ import Image from "next/image";
 const HeroBanner = ({ img }: { img: string }) => {
   return (
     <section className={styles.hero_banner_cont}>
-      <img src={img} alt="banner" />
+      <img src={img} className={styles.blured_edge} alt="banner" />
+      <div className={styles.main_img_cont}>
+        <img src={img} alt="banner" />
+      </div>
     </section>
   );
 };
